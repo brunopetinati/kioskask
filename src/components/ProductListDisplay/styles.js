8ff30df9a7abd864151a72df12f212248084b08c
Winiwon1@Z <brunopetinati@hotmail.com>
2023-01-09 16:19:43 -0300

    testando key

--- src/components/ProductListDisplay/styles.js
+++ src/components/ProductListDisplay/styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 312px;
   width: 100%;
-  background-color: purple;
+  background-color: blueviolet;
   margin-top: 10px;
 `;
